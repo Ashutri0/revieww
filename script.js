7aeb06a80fa01a62b44c526dcd91571300395f20
ASHUTOSH TRIPATHI <91529521+Ashutri0@users.noreply.github.com>
2024-04-10 00:44:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwxrr9gY3hHl86SuG0uv8nQa0NJHnmqFYqzCzrqM0w4wWDuE-B--j9r9iwch5FHZK6o/exec'
 
-const form = document.forms['Review-data']
+const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
